78c2496bceb75172e8655cc8bbd2336e54325d4c
Ahemao <andersonhenao329@gmail.com>
2024-12-28 00:20:41 -0500

    no se agregaron

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { IngredientesComponent } from './pages/ingredientes/ingredientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductosComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'equipo', component: EquipoComponent},
+  {path: 'ingredientes', component: IngredientesComponent},
+  {path: 'contactanos', component: ContactoComponent},
+  {path: '**', pathMatch: 'full', redirectTo: ''},
+];
 
 @NgModule({
+  // imports: [RouterModule.forRoot(routes, {useHash: true})],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FootrerComponent } from './shared/footrer/footrer.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { IngredientesComponent } from './pages/ingredientes/ingredientes.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { BntTopComponent } from './shared/bnt-top/bnt-top.component';
+import { AlertComponent } from './shared/alert/alert.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FootrerComponent,
+    ProductosComponent,
+    ServiciosComponent,
+    EquipoComponent,
+    IngredientesComponent,
+    ContactoComponent,
+    BntTopComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
